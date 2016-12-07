b2be45c27f3f62b0dc6168d6aa842e959e608b5c
xzhou29 <xin9186@gmail.com>
2016-12-07 10:33:07 -0600

    Update Assignment3COSC4353.java

--- Assignment3COSC4353/src/assignment3cosc4353/Assignment3COSC4353.java
+++ Assignment3COSC4353/src/assignment3cosc4353/Assignment3COSC4353.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+11/30/2016
+Team Members: Xin Zhou, Xin Zhang
  */
 package assignment3cosc4353;
 
